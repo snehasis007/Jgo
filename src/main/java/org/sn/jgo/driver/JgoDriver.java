8602b5daea46978e625c7fef7ccd1556b7b185e0
snehasis007 <snehasis007@gmail.com>
2024-08-10 13:56:09 -0400

    drivers

--- src/main/java/org/sn/jgo/driver/JgoDriver.java
+++ src/main/java/org/sn/jgo/driver/JgoDriver.java
@@ -12,32 +12,35 @@ import java.util.logging.Logger;
 public final class JgoDriver {
     private final static Logger logger = Logger.getLogger("Jgo");
     public static void main(String[] args) {
-        if(args !=null && args.length >0){
-            logger.info("arguments received"+ args[0]);
-            try{
-                readSourceFiles(args[0]);
-            }catch (IOException e){
+//        if(args !=null && args.length >0){
+//            logger.info("arguments received"+ args[0]);
+//
+//        }
+        try{
+            readSourceFiles("./sourcefiles");
+        }catch (IOException e){
 
-            }
         }
     }
 
     private static void readSourceFiles(String path) throws IOException{
         Path srcs = Path.of(path);
-        logger.info(srcs.toString());
-        Files.walk(srcs).filter(Files::isRegularFile).forEach((Path p)->{
-            File f= p.toFile();
-            try {
-                BufferedReader fs = Files.newBufferedReader(p);
-                fs.lines().forEach((String s)->{
-                    String[] st= s.split(JgoDriverConstants.whitespace_charclass);
-                    Arrays.asList(st).forEach((String sd)->{
-                        logger.info(sd);
-                    });
+        //logger.info(srcs.toString());
+        Files.walk(srcs).filter(Files::isRegularFile).distinct().forEach((Path p)->{
+            try{
+                if( p.toFile().getName().endsWith(".jgo")){
+                    BufferedReader fs = Files.newBufferedReader(p);
+                    fs.lines().forEach((String s)->{
+                        String[] st= s.split(JgoDriverConstants.whitespace_charclass);
+                        Arrays.asList(st).forEach((String sd)->{
+                            logger.info(sd);
+                        });
 
-                });
+                    });
+                }
+
             } catch (Exception e) {
-                throw new RuntimeException(e);
+
             }
         });
     }
